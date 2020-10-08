f6acb8a6d866b5dd4d32753036a184a69e8071b5
ADMIN <snandhini499@gmail.com>
2020-10-08 19:28:40 +0530

    Account details CRUD operations

--- Banking/src/main/java/com/example/Banking/model/Account.java
+++ Banking/src/main/java/com/example/Banking/model/Account.java
@@ -6,7 +6,7 @@ import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.GenerationType;
+
 @Entity
 @Table(name = "accountdetails")
 public class Account {
