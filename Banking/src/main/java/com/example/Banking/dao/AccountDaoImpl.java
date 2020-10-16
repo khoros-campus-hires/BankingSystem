d0df163476f5b1ee0561ba39f9fce6ef6dc87e72
ADMIN <snandhini499@gmail.com>
2020-10-16 15:11:35 +0530

    added dao class

---
+++ Banking/src/main/java/com/example/Banking/dao/AccountDaoImpl.java
@@ -0,0 +1,57 @@
+package com.example.Banking.dao;
+import com.example.Banking.model.Account;
+import com.example.Banking.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class AccountDaoImpl implements AccountDao {
+
+    @Autowired
+    private AccountRepository accountRepository;
+    private Account accountDetails;
+
+    @Override
+    public List<Account> getAll() {
+        return accountRepository.findAll();
+    }
+
+    public Optional<Account> getAccount(long id) {
+
+        return accountRepository.findById(id);
+    }
+
+    @Override
+    public void insertAccount(Account account) {
+        accountRepository.save(account);
+
+    }
+    //To delete account
+    @Override
+    public void deleteAccount(long id) {
+        accountRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateAccount(Account account, long AcNumber) {
+        Account accounts = accountRepository.findById(AcNumber).orElse(null);
+        //.orElseThrow(() -> new ResourceNotFoundException("Account not found for this id :: " + AcNumber));
+        //assert accounts != null;
+        accounts.setAccountNumber(accountDetails.getAccountNumber());
+        accounts.setAccountType(accountDetails.getAccountType());
+        accounts.setBankName(accountDetails.getBankName());
+        accounts.setBranchName(accountDetails.getBranchName());
+        accounts.setAccountBalance(accountDetails.getAccountBalance());
+        accounts.setIfscCode(accountDetails.getIfscCode());
+        accounts.setCifNumber(accountDetails.getCifNumber());
+
+
+        accountRepository.save(accounts);
+
+    }
+
+
+}
