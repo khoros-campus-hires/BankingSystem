2ec366563234ea6155b57efcfb26ad5d417ca284
Your Name <you@example.com>
2020-10-19 19:21:18 +0530

    testing for getAll method

--- Banking/src/main/java/com/example/Banking/Model/User.java
+++ Banking/src/main/java/com/example/Banking/Model/User.java
@@ -5,17 +5,21 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
         @Id
         private long id;
         private String firstName;
         private String lastName;
-        private Date dob;
+        //private Date dob;
         private int age;
         private String address;
         private long contact_number;
@@ -23,11 +27,7 @@ public class User {
 
 
 
-        @Override
-        public String toString() {
-            return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", Date of Birth=" + dob +", /n" +
-                    "age=" + age + "address=" + address +", contact_number=" + contact_number + ", email=" +email + "]";
-        }
+
 
 
 
---
+++ Banking/src/test/java/com/example/Banking/Controller/UserControllerTest.java
@@ -0,0 +1,60 @@
+package com.example.Banking.Controller;
+
+import com.example.Banking.Model.User;
+import com.example.Banking.Service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getAllUsersTest() throws Exception {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1212121212, "manas", "vini", 21, "10,chennai", 999999999, "m@mail.com"));
+        //users.add(new User(17,"vini"));
+        Mockito.when(userService.getAll()).thenReturn(users);
+
+        /*String url = "/Users/getUsers";
+        MvcResult mvcResult = mockMvc.perform(get(url)).andExpect(status().isOk()).andReturn();
+        String actualJsonResponse = mvcResult.getResponse().getContentAsString();
+        System.out.println(actualJsonResponse);
+        String expectedResponse = objectMapper.writeValueAsString(users);
+        Assertions.assertThat(actualJsonResponse).isEqualToIgnoringWhitespace(expectedResponse);*/
+        String url = "/Users/getUsers";
+        RequestBuilder rb = MockMvcRequestBuilders.get(url).accept(MediaType.APPLICATION_JSON);
+        MvcResult mvcResult = mockMvc.perform(rb).andReturn();
+        String actualResponse = mvcResult.getResponse().getContentAsString();
+
+        System.out.println(actualResponse);
+
+        String expectedResponse = objectMapper.writeValueAsString(users);
+        Assertions.assertThat(actualResponse).isEqualToIgnoringWhitespace(expectedResponse);
+    }
+}
+
