377a87d93a9f5a1e3782eb8d8b06ba16744d0c45
Gokul_khoros <gokulreko@gmail.com>
2020-10-22 15:32:33 +0530

    Now Transaction service have one status either SUCCESSFUL or FAILED.

--- Banking/src/main/java/com/example/Banking/service/TransactionService.java
+++ Banking/src/main/java/com/example/Banking/service/TransactionService.java
@@ -16,22 +16,32 @@ public class TransactionService {
 
     public String transaction(Transaction transaction_details) throws Exception {
         transactionDao.insertTransactionTable(transaction_details);
+        Account senderAccCheck = null;
+        try{ senderAccCheck = accountDao.getAccount(transaction_details.getFromAccount()); }
+        catch (Exception e) { transactionDao.updateTransactionTable(transaction_details,Transaction.State.FAILED); }
         Account senderAcc = accountDao.getAccount(transaction_details.getFromAccount());
-        Account recieverAcc = accountDao.getAccount(transaction_details.getToAccount());
 
-        if( senderAcc.getAccountBalance() > transaction_details.getTransferAmount()) {
+        Account receiverAccCheck = null;
+        try{ receiverAccCheck = accountDao.getAccount(transaction_details.getToAccount());}
+        catch (Exception e){ transactionDao.updateTransactionTable(transaction_details,Transaction.State.FAILED);}
+        Account receiverAcc = accountDao.getAccount(transaction_details.getToAccount());
+
+        if( (senderAcc.getAccountBalance() > transaction_details.getTransferAmount()) ) {
+
             senderAcc.setAccountBalance(senderAcc.getAccountBalance() - transaction_details.getTransferAmount());
             accountDao.updateAccount(senderAcc);
+            receiverAcc.setAccountBalance(receiverAcc.getAccountBalance() + transaction_details.getTransferAmount());
+            accountDao.updateAccount(receiverAcc);
             transactionDao.updateTransactionTable(transaction_details,Transaction.State.SUCCESSFUL);
-        }
 
+
+            return new TransactionStatus().returnMsg("Transaction Success");
+        }
         else {
             transactionDao.updateTransactionTable(transaction_details,Transaction.State.FAILED);
             return new TransactionStatus().returnMsg("Transaction Failed due to insufficient balance");
         }
 
-        recieverAcc.setAccountBalance(recieverAcc.getAccountBalance() + transaction_details.getTransferAmount());
-        accountDao.updateAccount(recieverAcc);
-        return new TransactionStatus().returnMsg("Transaction Success");
+
     }
 }
